b023907c6aeac94ccc70112779f3d2a1c8c35a72
Muhammad faisal <faisal@techservicesstudio.com>
2025-09-03 16:33:55 +0500

    express validator and conditions

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const route = express.Router();
+const { body, validationResult } = require("express-validator");
 route.get("/register", (req, res) => {
   res.render("register");
 });
-route.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("User registered successfully");
-});
+route.post(
+  "/register",
+  body("username").trim().isLength({ min: 3 }),
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 6 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ erros: errors.array(), message: "Invalid data" });
+    }
+    res.send(errors);
+  }
+);
 
 module.exports = route;
